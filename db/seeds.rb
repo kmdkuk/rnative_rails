3753911c8320f21d8d155e9aca6fef7bc6bd7817
kouki <kouworld0123@gmail.com>
2018-08-18 14:13:47 +0900

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/csv/boards.csv') do |row|
+  Board.create!(title: row[1], description: row[2])
+end
+
+CSV.foreach('db/csv/users.csv') do |row|
+  User.create!(name: row[1], sex: row[2], age: row[3])
+end
+
+CSV.foreach('db/csv/comments.csv') do |row|
+  Comment.create!(board_id: row[1], user_id: row[2], comment: row[3])
+end
